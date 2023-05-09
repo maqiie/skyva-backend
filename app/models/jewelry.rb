cfdaee3a56bf67dac748c233c9426e8614819c12
maqiie <99504010+maqiie@users.noreply.github.com>
2023-05-09 15:05:03 +0300

    worked on backend

---
+++ app/models/jewelry.rb
@@ -0,0 +1,2 @@
+class Jewelry < ApplicationRecord
+end
---
+++ db/migrate/20230509104658_create_jewelries.rb
@@ -0,0 +1,11 @@
+class CreateJewelries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jewelries do |t|
+      t.string :name
+      t.integer :price
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
